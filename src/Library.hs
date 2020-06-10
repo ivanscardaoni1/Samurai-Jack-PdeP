ac4237e5abbfc6ef0a387ca859bd443486eef6bd
ivanscardaoni1 <ivan.scardaoni1@gmail.com>
2020-06-10 17:26:07 -0300

    Commit parcial

--- src/Library.hs
+++ src/Library.hs
@@ -15,20 +15,8 @@ data Personaje = UnPersonaje {
                     elementos :: [Elemento],
                     anioPresente :: Number } deriving (Eq, Show)
 
-katanaMagica :: Elemento
-katanaMagica = UnElemento "Salvador" (modificarSalud (+ (-10))) (modificarSalud (+5)) 
-espadaMaldita :: Elemento
-espadaMaldita = UnElemento "Maldad" (modificarSalud (+ (-8))) (modificarSalud (+7))
-rayoLaser :: Elemento
-rayoLaser = UnElemento "Salvador" (modificarSalud (+ (-20))) (modificarSalud (+0))
-
-aku :: Personaje
-aku = UnPersonaje "Aku" 20 [espadaMaldita] 2020
-jack :: Personaje
-jack = UnPersonaje "Samurai Jack" 50 [katanaMagica, rayoLaser] 2025
-
-mandarAlAnio :: Personaje -> Number -> Personaje
-mandarAlAnio personaje anio = personaje {anioPresente = anio}
+mandarAlAnio :: Number -> Personaje -> Personaje
+mandarAlAnio anio personaje = personaje {anioPresente = anio}
 
 modificarSalud ::(Number -> Number) -> Personaje -> Personaje
 modificarSalud funcion personaje = personaje {salud = ((max 0.funcion) (salud personaje))}
@@ -51,5 +39,31 @@ esMortal personaje enemigo = (any (>= salud personaje) . map (danioQueProduce en
 enemigosMortales :: Personaje -> [Personaje] -> [Personaje] 
 enemigosMortales personaje = filter (esMortal personaje)
 
+aplicarVeces :: Number -> (a -> a) -> a -> a
+aplicarVeces 0 funcion valor = valor
+aplicarVeces cantidadDeVeces funcion valor = foldl (\valor funcion -> funcion valor) valor (replicar cantidadDeVeces funcion)
 
+replicar :: Number -> a -> [a]
+replicar 0 _ = []
+replicar cantidadDeVeces valor = map (\numero -> valor) [1..cantidadDeVeces]
 
+concentracion :: Number -> Elemento
+concentracion nivel = UnElemento "Magia" (modificarSalud (+0)) (aplicarVeces nivel meditar)
+
+esbirrosMalvados :: Number -> [Elemento]
+esbirrosMalvados cantidad = replicar cantidad (UnElemento "Maldad" (modificarSalud (+ (-1))) (modificarSalud (+0)))
+
+katanaMagica :: Elemento
+katanaMagica = UnElemento "Magia" (modificarSalud (+ (-1000))) (modificarSalud (+0)) 
+espadaMaldita :: Elemento
+espadaMaldita = UnElemento "Maldad" (modificarSalud (+ (-8))) (modificarSalud (+7))
+rayoLaser :: Elemento
+rayoLaser = UnElemento "Tecnologia Avanzada" (modificarSalud (+ (-20))) (modificarSalud (+0))
+
+jack :: Personaje
+jack = UnPersonaje "Samurai Jack" 300 [concentracion 3, katanaMagica] 200
+aku :: Number -> Number -> Personaje
+aku anio salud = UnPersonaje "Aku" salud (concentracion 4 : portalAlFuturo anio : esbirrosMalvados (anio*100)) anio
+
+portalAlFuturo :: Number -> Elemento
+portalAlFuturo anio = UnElemento "Magia" (mandarAlAnio (anio + 2800)) (mandarAlAnio (anio + 2800))
